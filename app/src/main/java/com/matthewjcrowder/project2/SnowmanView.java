1300610af511b4a39a222ecb979e91f47163fc64
Matt Crowder <mcrowder65@gmail.com>
2017-02-06 14:46:20 -0700

    drawing some black balls

--- app/src/main/java/com/matthewjcrowder/project2/SnowmanView.java
+++ app/src/main/java/com/matthewjcrowder/project2/SnowmanView.java
@@ -13,23 +13,47 @@ import android.view.View;
  */
 
 public class SnowmanView extends View {
+
+    Snowball bottom;
+    Snowball middle;
+    Snowball top;
+
     public SnowmanView(Context context, AttributeSet attrs) {
         super(context, attrs);
-    }
-
-    int x = 500;
-    int y = 500;
-    int r = 100;
-    Paint mPaint = new Paint();
-    @Override
-    public void onDraw(Canvas c)
-    {
         DisplayMetrics displaymetrics = new DisplayMetrics();
         ((Activity) getContext()).getWindowManager()
                 .getDefaultDisplay()
                 .getMetrics(displaymetrics);
         int height = displaymetrics.heightPixels;
         int width = displaymetrics.widthPixels;
-        c.drawCircle(height/2,width/2,r,mPaint);
+        Paint paint = new Paint();
+        bottom = new Snowball(width/2, height / 2, 100, paint);
+        middle = new Snowball(width / 2, height / 2 - 165, 75, paint);
+        top = new Snowball(width / 2, height / 2 - 275, 50, paint);
+    }
+
+
+    @Override
+    public void onDraw(Canvas c)
+    {
+        drawCircle(bottom, c);
+        drawCircle(middle, c);
+        drawCircle(top, c);
+    }
+
+    private void drawCircle(Snowball ball, Canvas c) {
+        c.drawCircle(ball.x, ball.y, ball.radius, ball.paint);
+    }
+    class Snowball {
+        public int x;
+        public int y;
+        public int radius;
+        public Paint paint;
+        Snowball(int x, int y, int radius, Paint paint) {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+            this.paint = new Paint(paint);
+        }
     }
 }
